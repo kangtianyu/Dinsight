c91ef3d102a611658a0f24d18ffb8a8911a134d2
kangtianyu <kangtianyu@hotmail.com>
2016-04-29 03:19:53 -0400

    Update KNN_basic.java

--- src/KNN_basic.java
+++ src/KNN_basic.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map.Entry;
 import java.util.Random;
+import java.util.TreeMap;
 
 
 public class KNN_basic {
@@ -17,6 +19,9 @@ public class KNN_basic {
 	private static boolean use_fixed_seed;
 	private static HashMap<String, ArrayList<String[]>> datasets;
 	private static String[] features;
+	private static HashMap<String, HashMap<String, String>> sid_step_cfa;
+	private static HashMap<String, TreeMap<Double, String>> sortedCorrelations;
+	private static HashMap<String, Double> userMean;
 
 	private static void load_data() {		
 		if(force_reprepare_data||!loadData("KNN_train")||!loadData("KNN_valid")||!loadData("KNN_test")){
@@ -97,16 +102,40 @@ public class KNN_basic {
 		bw.write("\n");		
 	}
 
-	private static void computeCorrelations() {
-		HashMap<String, HashMap<String,String>> sid_step_cfa = reconstructData("KNN_train");
+	private static void computeUserMeans(String datasetName) {
+		userMean  = new HashMap<String,Double>();
+		HashMap<String, Integer> userMeanNum = new HashMap<String,Integer>();
+		ArrayList<String[]> trainData = datasets.get(datasetName);
+		String step,cfa;
+		for(String[] tmp:trainData){
+			step = tmp[5];cfa = tmp[13];
+			if(!userMean.containsKey(step)){
+				userMean.put(step, 0.0);
+				userMeanNum.put(step, 0);
+			}
+			userMean.put(step, userMean.get(step)+Double.valueOf(cfa));
+			userMeanNum.put(step, userMeanNum.get(step)+1);
+		}
+		for(String tmp:userMean.keySet()) userMean.put(tmp, userMean.get(tmp)/userMeanNum.get(tmp));
+		
+	}
+
+	private static void computeCorrelations(String trainDataName,double alpha,double delta,double gamma) {
+		sid_step_cfa = reconstructData(trainDataName);
+		sortedCorrelations = new HashMap<String, TreeMap<Double,String>>();
 		HashMap<String, HashMap<String,Double>> correlations = new HashMap<String, HashMap<String,Double>>();
-		for(String s1:sid_step_cfa.keySet()) correlations.put(s1, new HashMap<String,Double>());
+		for(String s1:sid_step_cfa.keySet()){
+			correlations.put(s1, new HashMap<String,Double>());
+			sortedCorrelations.put(s1, new TreeMap<Double,String>());
+		}
 		for(String s1:sid_step_cfa.keySet()){
 			for(String s2:sid_step_cfa.keySet()){
 				if(!correlations.get(s1).containsKey(s2)) {
-					Double correlation = getCorrelation(sid_step_cfa,s1,s2,12.9,6.2,-1.9);
+					Double correlation = getCorrelation(sid_step_cfa,s1,s2,alpha,delta,gamma);
 					correlations.get(s1).put(s2, correlation);
 					correlations.get(s2).put(s1, correlation);
+					sortedCorrelations.get(s1).put(correlation, s2);
+					sortedCorrelations.get(s2).put(correlation, s1);
 				}
 			}	
 		}		
@@ -114,7 +143,7 @@ public class KNN_basic {
 	
 	private static double getCorrelation(HashMap<String, HashMap<String, String>> sid_step_cfa, String s1, String s2,double alpha,double delta,double gamma) {
 		HashMap<String, String> set_s1 = sid_step_cfa.get(s1);
-		HashMap<String, String> set_s2 = sid_step_cfa.get(s1);
+		HashMap<String, String> set_s2 = sid_step_cfa.get(s2);
 		HashSet<String> set_s1s2 = new HashSet<String>();
 		for(String step:set_s1.keySet()) if(set_s2.keySet().contains(step)) set_s1s2.add(step);
 		double us1 = 0, us2 = 0;
@@ -124,16 +153,20 @@ public class KNN_basic {
 		}
 		us1 /= set_s1s2.size();
 		us2 /= set_s1s2.size();
-		double d1 = 0, d2 = 0, d3 = 0;
+		double d1 = 0.0, d2 = 0.0, d3 = 0.0;
 		for(String step:set_s1s2){
 			d1 += (Double.valueOf(set_s1.get(step)) - us1) * (Double.valueOf(set_s2.get(step)) - us2);
 			d2 += (Double.valueOf(set_s1.get(step)) - us1) * (Double.valueOf(set_s1.get(step)) - us1);
 			d3 += (Double.valueOf(set_s2.get(step)) - us2) * (Double.valueOf(set_s2.get(step)) - us2);
 		}
-		double d4 = 1/(set_s1s2.size()-1);
+		double d4 = set_s1s2.size()>1?1.0/(set_s1s2.size()-1):1;
 		double rho = (d4*d1) / (Math.sqrt(d4*d2)*Math.sqrt(d4*d3));
+		if(rho != rho) rho = 1.0;
 		double rho_bar = (set_s1s2.size()*rho)/(set_s1s2.size()+alpha);
-		double rho_tilde = -1/Math.expm1(-delta*rho_bar-gamma);
+		double rho_tilde = -1.0/Math.expm1(-delta*rho_bar-gamma);
+		if(rho_tilde != rho_tilde){
+			System.out.println(rho);
+		}
 		return rho_tilde;
 	}
 
@@ -149,6 +182,37 @@ public class KNN_basic {
 		return data;		
 	}
 
+	private static void predic_KNN(int k, String datasetName,double beta) {
+		ArrayList<String[]> data = datasets.get(datasetName);
+		String sid,step,cfa;
+		double error=0;
+		int num=0;
+		for(String[] tmp:data){
+			sid = tmp[1];step = tmp[5];cfa = tmp[13];
+			TreeMap<Double, String> s = sortedCorrelations.get(sid);
+			Entry<Double, String> entry = s!=null?s.lastEntry():null;
+			String s_tilde;
+			double rho_tilde,d1 = 0, d2 = 0;
+			int n=0;
+			while(n<k && entry!=null){
+				rho_tilde = entry.getKey();
+				s_tilde = entry.getValue();
+				if(sid_step_cfa.get(s_tilde).containsKey(step)){
+					d1 += rho_tilde * Double.valueOf(sid_step_cfa.get(s_tilde).get(step));
+					d2 += Math.abs(rho_tilde);
+					n++;
+				}
+				entry = s.lowerEntry(rho_tilde);
+			}
+			double c_tilde = d2!=0?d1/d2:1;
+			double c_hat = userMean.containsKey(step)?(c_tilde*d2+userMean.get(step)*beta)/(d2+beta):c_tilde;
+			error += (Double.valueOf(cfa) - c_hat) * (Double.valueOf(cfa) - c_hat);
+			num++;
+		}
+		error /= num;
+		System.out.println("K = " + k + " , RMSE = " + error);		
+	}
+
 	public static void main(String[] args) {
 		// configurations
 		workingDirectory = System.getProperty("user.dir");
@@ -157,12 +221,22 @@ public class KNN_basic {
 		
 		// initialize
 		datasets = new HashMap<String, ArrayList<String[]>>();
+		double alpha = 12.9,beta = 1.5,delta = 6.2,gamma = -1.9;
 		
 		// load data
 		load_data();
+		System.out.println("Data loaded");
+		
+		// compute user mean
+		computeUserMeans("KNN_train");
+		System.out.println("User mean computed");
 		
 		// compute correlations
-		computeCorrelations();
+		computeCorrelations("KNN_train",alpha,delta,gamma);
+		System.out.println("Correlations computed");
+		
+		// validate
+		for(int k=1;k<=100;k++)	predic_KNN(k,"KNN_valid",beta);
 		
 	
 	}
